134e63a04af16c760937926c43238bbb0c6b0cd8
tattobu28 <tattobu3989@gmail.com>
2021-10-30 23:50:41 +0900

    テストコード修正・追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :encrypted_password, :last_name, :first_name, :last_name_kana,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana,
                                              :first_name_kana, :birth_date])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, length: { maximum: 40 }
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥々ー]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
