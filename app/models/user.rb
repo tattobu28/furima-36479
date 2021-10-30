69aa4e84f7c9743fd779d154a0a0fc167c6372bf
tattobu28 <tattobu3989@gmail.com>
2021-10-30 20:50:59 +0900

    テストコード導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nickname, presence: true, length: { maximum: 40 }
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ンー-龥]/ }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
+  validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } 
+  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Lorem.characters(number:6,min_alpha:3,min_numeric:3)}
+    password_confirmation {password}
+    last_name             { person.last.kanji }
+    first_name            { person.first.kanji }
+    last_name_kana        { person.last.katakana }
+    first_name_kana       { person.first.katakana }
+    birth_date            {Faker::Date.between(from: '1930-01-01', to: '2016-12-31')}
+  end
+end
