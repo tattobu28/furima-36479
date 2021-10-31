4d0006c90b30fda03355def3dcc5ffd58432b45d
tattobu28 <tattobu3989@gmail.com>
2021-10-31 17:19:44 +0900

    active storageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
