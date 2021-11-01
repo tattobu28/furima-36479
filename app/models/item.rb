946ce4043e0256d3dbfb39ccd2d0a6628817c0d3
tattobu28 <tattobu3989@gmail.com>
2021-11-01 17:59:26 +0900

    priceのvalidationとprice.jsの準備

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,10 @@ class Item < ApplicationRecord
   validates :shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price, presence: true
+  
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                      presence: { message: "can't be blank" }
+  end
 
 end
