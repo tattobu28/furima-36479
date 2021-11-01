d1e481d0e9302fdca9c64458b8db5a3322a362da
tattobu28 <tattobu3989@gmail.com>
2021-11-01 17:11:20 +0900

    セレクトボックス実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,8 +7,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = items.new(item_params)
-    @item.save
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
