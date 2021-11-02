1371a5073086c8a730488ad6d32e85b748a3ab98
tattobu28 <tattobu3989@gmail.com>
2021-11-02 22:49:10 +0900

    出品可能に実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :nickname, presence: true, length: { maximum: 40 }
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
   validates :info, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :sales_status_id, numericality: { other_than: 1, message: "can't be blank" }
