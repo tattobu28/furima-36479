7cf482ea70ddd8de6fee833f07bddf22ecc3c582
tattobu28 <tattobu3989@gmail.com>
2021-11-04 20:16:27 +0900

    指摘箇所修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,8 +37,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
+      it 'カテゴリーに「--」が選択されている場合は出品できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
       it '商品の状態が空では出品できない' do
         @item.sales_status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Sales status can't be blank")
+      end
+      it '商品の状態に「--」が選択されている場合は出品できない' do
+        @item.sales_status_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Sales status can't be blank")
       end
@@ -47,13 +57,28 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
       end
+      it '配送料の負担に「--」が選択されている場合は出品できない' do
+        @item.shipping_fee_status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
+      end
       it '発送元の地域が空では出品できない' do
         @item.prefecture_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
+      it '発送元の地域に「--」が選択されている場合は出品できない' do
+        @item.prefecture_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
       it '発送までの日数が空では出品できない' do
         @item.scheduled_delivery_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
+      end
+      it '発送までの日数に「--」が選択されている場合は出品できない' do
+        @item.scheduled_delivery_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include("Scheduled delivery can't be blank")
       end
