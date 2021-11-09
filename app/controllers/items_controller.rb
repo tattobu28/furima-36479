c506b818c659c0ca1ac514f19d99dbb7c8445875
tattobu28 <tattobu3989@gmail.com>
2021-11-09 22:53:53 +0900

    商品編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,12 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path(item.id)
+  end
+
   private
 
   def item_params
