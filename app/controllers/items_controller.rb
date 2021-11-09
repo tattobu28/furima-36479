ef61a3d399662198b9eef9f49c063850c179d1ab
tattobu28 <tattobu3989@gmail.com>
2021-11-09 23:40:58 +0900

    セキュリティ面実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,12 +24,18 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to item_path(item.id)
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(item.id)
+    else
+      render :edit
+    end
   end
 
   private
