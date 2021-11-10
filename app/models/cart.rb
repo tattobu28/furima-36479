ebad7f3bff5732504006ab0fba64fed0c7c539d3
tattobu28 <tattobu3989@gmail.com>
2021-11-10 23:40:14 +0900

    アソシエーション記述

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,5 @@
 class Cart < ApplicationRecord
+  belongs_to :item
+  has_one :address
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :items
+  has_many :carts
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
