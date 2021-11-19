5ead5d5b7af64ed60884ae419d74621ea57a195c
tattobu28 <tattobu3989@gmail.com>
2021-11-19 21:54:40 +0900

    rubocop実行

--- app/models/cart_address.rb
+++ app/models/cart_address.rb
@@ -7,7 +7,7 @@ class CartAddress
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :addresses
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/}
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/ }
     validates :user_id
     validates :item_id
     validates :token
